f894b20ac92da2159a400ca91d9277d0def8b5a1
tourn <latzer.daniel@gmail.com>
2018-03-21 21:53:17 +0100

    Add excuse generator to index view

--- src/controllers/api.ts
+++ src/controllers/api.ts
@@ -1,7 +1,16 @@
 "use strict";
 
 import { Response, Request } from "express";
+import {heroes as heroList} from '../data/heroes';
+import {ExcuseGenerator} from "../analyzers/excuse.analyzer";
 
 export let getApi = (req: Request, res: Response) => {
   res.sendStatus(200);
+};
+
+const heroes = heroList.map(h => h.localized_name);
+const generator = new ExcuseGenerator(heroes, heroes);
+
+export let excuse = (req: Request, res: Response) => {
+  res.send(200, generator.randomExcuse());
 };
--- src/server.ts
+++ src/server.ts
@@ -45,7 +45,6 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(expressValidator());
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(flash());
 app.use(lusca.xframe("SAMEORIGIN"));
 app.use(lusca.xssProtection(true));
 app.use(express.static(path.join(__dirname, "public"), {maxAge: 31557600000}));
@@ -121,7 +120,7 @@ app.get("/", dashboard.index);
 /**
  * API examples routes.
  */
-app.get("/api", apiController.getApi);
+app.get("/excuse", apiController.excuse);
 
 /**
  * Error Handler. Provides full stack - remove for production
